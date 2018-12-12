356f06b3802577ee02d1a4680640d11d3cc09d45
Gary Baril <Gary.E.Baril@lowes.com>
2018-12-12 08:20:22 -0500

    Test

---
+++ SpecflowDotNetCoreVue.Spec/Steps/LoginSteps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpecflowDotNetCoreVue.Spec.Steps
+{
+    [Binding]
+    public class LoginSteps
+    {
+        [Given(@"a sales Id of (.*)")]
+        public void GivenASalesIdOf(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"a password '(.*)'")]
+        public void GivenAPassword(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"a password ""(.*)""")]
+        public void GivenAPassword(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I login")]
+        public void WhenILogin()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the system should retun a User Ojbect")]
+        public void ThenTheSystemShouldRetunAUserOjbect()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should be shown a friendly erorr message")]
+        public void ThenIShouldBeShownAFriendlyErorrMessage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
